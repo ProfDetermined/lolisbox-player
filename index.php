26f57546a4dd35c243413779b56f653f063e666b
Detzz <a2z.team.198@gmail.com>
2021-09-12 00:32:29 +0700

    add lolis api call

--- index.php
+++ index.php
@@ -6,19 +6,34 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FastPlayer5 | Detzz</title>
+    <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
     <?php
         if (substr($link, -3) == "wav") {
-            echo "<audio controls>";
-            echo "<source src = '".$link."' type='audio/wav'>";
+            echo "<audio controls id='playr'>";
+            echo "<source src = '" . $link . "' type='audio/wav'>";
             echo "</audio>";
         }
+
+        if ($link == "https://box.lolis.love/*") {
+            $api = $link + 'meta';
+            $call = json_decode($api,true);
+
+            echo "<h1>";
+            echo $call;
+            echo "</h1>";
+        }
     ?>
+
+    
 </body>
+
 </html>
