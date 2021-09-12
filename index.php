578b5503eb3161071bcfa811d165c8eb4be97b96
Detzz <a2z.team.198@gmail.com>
2021-09-13 06:05:10 +0700

    add title reanme

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $lode = json_decode($local);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FastPlayer5 | Detzz</title>
+    <title><?php echo $call['globalMeta']['originFilename']; ?> | FastPlayer5</title>
     <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
