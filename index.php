7e55ba49a56e0416cf489690509b8b5cd8669242
Detzz <a2z.team.198@gmail.com>
2021-09-13 10:38:27 +0700

    add index landing

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 $home = "https://box.lolis.love/0/";
-$id = $_GET['id'];
-$link = $home . $id;
-
-$meta = "/meta";
-$a = $link . $meta;
-$api = file_get_contents($a);
-$call = json_decode($api, true);
 
 $local = file_get_contents('./content/db/song.json');
 $lode = json_decode($local);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,35 +12,12 @@ $lode = json_decode($local);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $call['globalMeta']['originFilename']; ?> | FastPlayer5</title>
+    <title>FastPlayer5</title>
     <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php
-    if (substr($link, -3) == "wav") {
-        echo "<audio controls autoplay id='mplay' class='hidden'>";
-        // autoplay
-        echo "<source src = '" . $link . "' type='audio/wav'>";
-        echo "</audio>";
-    } else if (substr($link, -13) == "videoplayback") {
-        echo "<script>document.getElementById('mcon').classList.add('hidden');</script>";
-        echo "<center style='padding: 1rem; padding-bottom: 0;'>";
-        echo "<video autoplay id='mplay'>";
-        echo "<source src='". $link ."' type='video/mp4'>";
-        echo "</video>";
-        echo "</center>";
-    } else if (substr($link, -3) == "mp4") {
-        echo "<script>document.getElementById('mcon').classList.add('hidden');</script>";
-        echo "<center style='padding: 1rem; padding-bottom: 0;'>";
-        echo "<video autoplay id='mplay'>";
-        echo "<source src='" . $link . "' type='video/mp4'>";
-        echo "</video>";
-        echo "</center>";
-    }
-    ?>
-
     <div class="music" id="mcon">
         <center>
             <?php
@@ -56,7 +27,7 @@ $lode = json_decode($local);
                     echo $lode->id . ". " . "[" . $lode->album . "] " . $lode->artist . " - " . $lode->song;
                     echo "</button>";
                 } else {
-                    echo "<a href='/?id=" . $lode->link_id . "'>";
+                    echo "<a href='/engine.php?id=" . $lode->link_id . "'>";
                     echo "<button type='button' class='btn btn-success'>";
                     echo $lode->id . ". " . "[" . $lode->album . "] " . $lode->artist . " - " . $lode->song;
                     echo "</button>";
@@ -66,19 +37,16 @@ $lode = json_decode($local);
                 echo "<br>";
             }
             ?>
-            <br><br><br><br>
         </center>
     </div>
 
-    <div class="container-fluid fixed_ft">
+    <div class="fixed_ft container-fluid">
         <div class="row" style="top: 50%;">
             <div class="col-md-2 col-sm-12 detailed" style="text-align: center;">
-                <a class="song-name" style="text-align: center;">
-                    <?php
-                    echo $call['globalMeta']['originFilename'];
-                    ?>
+                <a class="song-name" style="text-align: center; width: 100%;">
+                    Nothing Playing Now..
                 </a>
-                <div class="col-sm-12">
+                <div class="col-sm-12 col-md-0">
                     &nbsp;
                 </div>
             </div>
@@ -120,7 +88,7 @@ $lode = json_decode($local);
             </div>
         </div>
     </div>
-    <script src="./content/js/play.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
