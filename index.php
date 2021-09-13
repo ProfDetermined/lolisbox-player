5a86ef8c3876e5f394f1746b87058ad395242c7c
Detzz <a2z.team.198@gmail.com>
2021-09-13 11:15:53 +0700

    change logo

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $lode = json_decode($local);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FastPlayer5</title>
     <link rel="stylesheet" href="index.css">
-    <link rel="shortcut icon" href="https://box.lolis.love/0/m27hu.jpg" type="image/x-icon">
+    <link rel="shortcut icon" href="./content/svg/play.svg" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
